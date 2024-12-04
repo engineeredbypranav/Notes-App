1c2b32d62ab5f2bb63ded2bb0fb6bc260dbdb500
engineeredbypranav <pranav.82998@gmail.com>
2024-12-05 01:27:15 +0530

    Basic Structring and Navbar implementation done on frontend

---
+++ Frontend/PNotes/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import SignUp from './pages/SignUp/SignUp'
+import { BrowserRouter , Routes , Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+  <BrowserRouter>
+  <Navbar/>
+  <Routes>
+
+  <Route path = "/" element = {<Home/>} />
+  <Route path = "/login" element = {<Login/>} />
+  <Route path = "/signup" element = {<SignUp/>} />
+
+  </Routes>
+  
+  </BrowserRouter>
+  )
+}
+
+export default App
---
+++ Frontend/PNotes/src/components/Cards/ProfileInfo.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ProfileInfo = ({onLogout}) => {
+  return (
+    <div className='flex items-center gap-3'>
+
+    <div className='w-12 h-12 flex items-center justify-center rounded-full
+    text-slate-950 font-medium bg-slate-100'>PP</div>
+    <div>
+        <p className='text-sm font-medium'>Pranav</p>
+    </div>
+    
+    <button className='text-sm bg-red-500 p-1.5 rounded-md text-white hover:opacity-80'
+    onClick={onLogout}>Logout</button>
+    
+    </div>
+  )
+}
+
+export default ProfileInfo
