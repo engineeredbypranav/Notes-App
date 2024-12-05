e9d29741149496c698be6e539cfb8db2f4507695
engineeredbypranav <pranav.82998@gmail.com>
2024-12-06 02:10:10 +0530

    Made some changes in the login page

--- Frontend/PNotes/src/pages/Login/Login.jsx
+++ Frontend/PNotes/src/pages/Login/Login.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PasswordInput from '../../components/Input/PasswordInput'
 
 const Login = () => {
+
+  const [email , setEmail] = useState("")
+  const [password , setPassword] = useState("")
+  const [error , setError] = useState("")
+  
   return (
-    <div>Login</div>
+    <div className='flex items-center justify-center mt-28'>
+      <div className='w-96 border rounded bg-white px-7 py-10'>  
+        <form>
+          <h4 className='text 2xl mb-7'>Login 1:22:15</h4>
+
+          <input type= "text" placeholder='Email' className='input-box' value = {email}
+          onChange={(e) => setEmail(e.target.value) } />
+
+          <PasswordInput/>
+
+        </form>
+      </div>
+
+
+
+
+
+    </div>
   )
 }
 
